adafd3a5b069b54719485737f756388243be1d8b
Anish kumar dubey <anishkumardubey@Anishs-MacBook-Pro-2.local>
2024-04-27 16:14:56 +0530

    init

--- variablesLearning/variablesLearning.go
+++ variablesLearning/variablesLearning.go
@@ -155,6 +155,43 @@ That said, you cannot declare a constant that can only be computed at run-time l
 const currentTime = time.Now()
 */
 
+/*
+FORMATTING STRINGS IN GO
+
+Go follows the printf tradition from the C language.
+
+fmt.Printf - Prints a formatted string to standard out.
+fmt.Sprintf() - Returns the formatted string
+
+DEFAULT REPRESENTATION
+The %v variant prints the Go syntax representation of a value, it's a nice default.
+
+s := fmt.Sprintf("I am %v years old", 10)
+// I am 10 years old
+
+s := fmt.Sprintf("I am %v years old", "way too many")
+// I am way too many years old
+
+If you want to print in a more specific way, you can use the following formatting verbs:
+
+STRING
+s := fmt.Sprintf("I am %s years old", "way too many")
+// I am way too many years old
+
+INTEGER
+s := fmt.Sprintf("I am %d years old", 10)
+// I am 10 years old
+
+FLOAT
+s := fmt.Sprintf("I am %f years old", 10.523)
+// I am 10.523000 years old
+// The ".2" rounds the number to 2 decimal places
+s := fmt.Sprintf("I am %.2f years old", 10.523)
+// I am 10.52 years old
+
+
+*/
+
 func main() {
 	variableDeclaration()
 	shortVariableDeclaration()
