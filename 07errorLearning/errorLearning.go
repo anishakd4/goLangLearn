1c6151e2d9e43bdafe79d2a1a469e00a08ef70a8
Anish kumar dubey <anishkumardubey@Anishs-MacBook-Pro-2.local>
2024-04-29 07:40:08 +0530

    init

--- 07errorLearning/errorLearning.go
+++ 07errorLearning/errorLearning.go
@@ -11,6 +11,23 @@ An Error is any type that implements the simple built-in error interface:
 type error interface {
     Error() string
 }
+
+When something can go wrong in a function, that function should return an error as its last return value. Any code that calls a function that can
+return an error should handle errors by testing whether the error is nil.
+
+// Atoi converts a stringified number to an integer
+i, err := strconv.Atoi("42b")
+if err != nil {
+    fmt.Println("couldn't convert:", err)
+    // because "42b" isn't a valid integer, we print:
+    // couldn't convert: strconv.Atoi: parsing "42b": invalid syntax
+    // Note:
+    // 'parsing "42b": invalid syntax' is returned by the .Error() method
+    return
+}
+// if we get here, then
+// i was converted successfully
+
 */
 func sendSMSToCouple(msgToCustomer, msgToSpouse string) (int, error) {
 	costForCustomer, err := sendSMS(msgToCustomer)
@@ -85,7 +102,6 @@ The Go standard library provides an "errors" package that makes it easy to deal 
 
 var err error = errors.New("something went wrong")
 */
-
 
 func divide2(x, y float64) (float64, error) {
 	if y == 0 {
